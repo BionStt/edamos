cc4851ea09c904010ecaa3536865d9c726542b9b
Ihar Yakimush <IharYakimush@gmail.com>
2018-06-27 12:50:48 +0300

    redis data protection

--- src/Edamos.AspNetCore/EdamosWebHost.cs
+++ src/Edamos.AspNetCore/EdamosWebHost.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using Edamos.Core.Logs;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 namespace Edamos.AspNetCore
 {
     public static class WebHostExtensions
-    {       
+    {
+        private const int DataProtectionRedisDatabase = 1993774;
+
         public static IWebHostBuilder AddEdamosDefault(this IWebHostBuilder builder, string[] args)
         {
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
@@ -28,6 +34,15 @@ namespace Edamos.AspNetCore
 
             builder.ConfigureServices(services =>
             {
+                // OPTIONAL: change redis connection if needed
+                ConfigurationOptions redisOptions = new ConfigurationOptions ();
+                redisOptions.EndPoints.Add("redisdp", 6379);
+
+                ConnectionMultiplexer redis =
+                    ConnectionMultiplexer.Connect(redisOptions, Console.Out);
+
+                //TODO: add cert encryption
+                services.AddDataProtection().PersistKeysToRedis(redis).SetApplicationName(AppDomain.CurrentDomain.FriendlyName);
                 services.AddMvc();
                 services.AddLogging(logsBuilder => logsBuilder.AddEdamosLogs(config));
             });
