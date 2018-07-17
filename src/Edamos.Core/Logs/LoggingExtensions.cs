eb8d05aaf3a442eb2168e4507e174b1210a2ddb9
Ihar Yakimush <IharYakimush@gmail.com>
2018-07-17 18:24:21 +0300

    improve logging

--- src/Edamos.Core/Logs/LoggingExtensions.cs
+++ src/Edamos.Core/Logs/LoggingExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
 using Serilog.Sinks.Elasticsearch;
 
 namespace Edamos.Core.Logs
@@ -10,19 +12,82 @@ namespace Edamos.Core.Logs
     {
         public static ILoggingBuilder AddEdamosLogs(this ILoggingBuilder loggingBuilder, IConfiguration configuration)
         {
-            LoggerConfiguration loggerConfiguration = new LoggerConfiguration();
+            LoggerConfiguration conf = new LoggerConfiguration();
 
             //TODO: use real ELK Uri
             ElasticsearchSinkOptions sinkOptions =
-                new ElasticsearchSinkOptions(new Uri(DebugConstants.ElasticSearch.LoggingUri));        
+                new ElasticsearchSinkOptions(new Uri(DebugConstants.ElasticSearch.LoggingUri));
+            
+
             configuration.Bind(nameof(ElasticsearchSinkOptions), sinkOptions);
 
-            loggerConfiguration.WriteTo.Elasticsearch(sinkOptions);
+            conf.WriteTo.Elasticsearch(sinkOptions);
+            conf.Enrich.FromLogContext();
+            conf.Enrich.WithProperty("app", AppDomain.CurrentDomain.FriendlyName);
+            conf.Enrich.WithProperty("machine", Environment.MachineName);
+
+            conf.WriteTo.Logger(lc =>
+            {
+                lc.NewFilterBuilder()
+                    .AddSource("Microsoft.EntityFrameworkCore")
+                    .RemoveLevelsFromSource("Microsoft.EntityFrameworkCore", LogEventLevel.Debug, LogEventLevel.Verbose)
+                    .BuildIncludeOnly();
+
+            }).WriteToRollingFile("efcore.log");
+
+            conf.WriteTo.Logger(lc =>
+            {
+                lc.NewFilterBuilder()
+                    .AddSource("Microsoft.AspNetCore")
+                    .RemoveLevelsFromSource("Microsoft.AspNetCore", LogEventLevel.Debug, LogEventLevel.Verbose)
+                    .BuildIncludeOnly();
+
+            }).WriteToRollingFile("aspnetcore.log");            
+
+            conf.WriteTo.Logger(lc =>
+            {
+                lc.NewFilterBuilder()
+                    .AddSource("Microsoft.EntityFrameworkCore") // don't log "Microsoft.EntityFrameworkCore" source by default
+                    .RemoveLevelsFromSource("Microsoft.EntityFrameworkCore", // keep important events from "Microsoft.EntityFrameworkCore"
+                        LogEventLevel.Warning, 
+                        LogEventLevel.Error,
+                        LogEventLevel.Fatal)
+
+                    .AddSource("Microsoft.AspNetCore") // don't log "Microsoft.AspNetCore" source by default
+                    .RemoveLevelsFromSource("Microsoft.AspNetCore", // keep important events from "Microsoft.AspNetCore"
+                        LogEventLevel.Warning,
+                        LogEventLevel.Error,
+                        LogEventLevel.Fatal)
+                    .RemoveLevelsFromSource("Microsoft.AspNetCore.DataProtection", // keep important events from "Microsoft.AspNetCore.DataProtection"
+                        LogEventLevel.Information,
+                        LogEventLevel.Warning,
+                        LogEventLevel.Error,
+                        LogEventLevel.Fatal)
+                    .BuildExclude();
 
 #if DEBUG
-            loggerConfiguration.WriteTo.Console();
+                lc.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3} {TraceIdentifier,-30}] {Source}:{EId}:{EName} {NewLine}{Exception}");
 #endif
-            Log.Logger = loggerConfiguration.CreateLogger();
+                lc.WriteTo.Elasticsearch(sinkOptions);
+            });
+                
+            Log.Logger = conf.CreateLogger();
+
+            Log.Logger.Information("Startup. {@env}", new Dictionary<string, object>
+            {
+                {nameof(Environment.UserName), Environment.UserName},
+                {nameof(Environment.MachineName), Environment.MachineName},
+                {nameof(Environment.CommandLine), Environment.CommandLine},
+                {nameof(Environment.CurrentDirectory), Environment.CurrentDirectory},
+                {nameof(Environment.StackTrace), Environment.StackTrace},
+                {nameof(Environment.SystemDirectory), Environment.SystemDirectory},
+                {nameof(Environment.UserDomainName), Environment.UserDomainName},
+                {nameof(Environment.OSVersion), Environment.OSVersion.VersionString},
+                {nameof(Environment.Is64BitOperatingSystem), Environment.Is64BitOperatingSystem},
+                {nameof(Environment.Is64BitProcess), Environment.Is64BitProcess},
+                {nameof(Environment.ProcessorCount), Environment.ProcessorCount},
+                {nameof(Environment.UserInteractive), Environment.UserInteractive},
+            });
 
             loggingBuilder.AddSerilog(dispose: true);
             return loggingBuilder;
---
+++ src/Edamos.Core/Logs/SerilogConfiguration.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
+
+namespace Edamos.Core.Logs
+{
+    public static class SerilogConfiguration
+    {
+        public static LoggerConfiguration WriteToRollingFile(this LoggerConfiguration lc, string fileName)
+        {
+            if (fileName == null) throw new ArgumentNullException(nameof(fileName));
+
+            return lc.WriteTo.RollingFile(
+                Path.Combine("Logs", fileName),
+                outputTemplate:
+                "[{Timestamp:HH:mm:ss} {Level:u3}] {EventId} {Message:lj} {Properties}{NewLine}{Exception}");
+        }
+
+        public static LoggerConfiguration FilterIncludeOnlyCategory(this LoggerConfiguration lc, string category,
+            params LogEventLevel[] excludeLevels)
+        {
+            return lc.Filter.ByExcluding(e => excludeLevels.Contains(e.Level))
+                .Filter.ByIncludingOnly(Matching.FromSource(category));
+        }
+
+        public static LoggerConfiguration FilterExcludeLevelsFromCategory(this LoggerConfiguration lc, 
+            string category,
+            Func<LogEvent,bool> include,
+                params LogEventLevel[] excludeLevels)
+        {
+            return lc.Filter.ByExcluding(
+                e => excludeLevels.Contains(e.Level) && Matching.FromSource(category).Invoke(e));
+        }
+
+        public static FilterBuilder NewFilterBuilder(this LoggerConfiguration lc)
+        {
+            return new FilterBuilder(lc);
+        }
+
+        public static FilterBuilder AddSource(this FilterBuilder builder, string source)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            builder.AddCollection.Add(Matching.FromSource(source));
+
+            return builder;
+        }
+
+        public static FilterBuilder RemoveLevelsFromSource(this FilterBuilder builder, string source, params LogEventLevel[] levels)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            builder.RemoveCollection.Add(e => Matching.FromSource(source).Invoke(e) && levels.Contains(e.Level));
+
+            return builder;
+        }
+
+        public class FilterBuilder
+        {
+            private readonly LoggerConfiguration _lc;
+
+            public FilterBuilder(LoggerConfiguration lc)
+            {
+                _lc = lc ?? throw new ArgumentNullException(nameof(lc));
+            }
+
+            public readonly List<Func<LogEvent,bool>> RemoveCollection = new List<Func<LogEvent, bool>>();
+
+            public readonly List<Func<LogEvent,bool>> AddCollection = new List<Func<LogEvent, bool>>();
+
+            public LoggerConfiguration BuildExclude()
+            {
+                _lc.Filter.ByExcluding(e =>
+                {
+                    bool result = this.RemoveCollection.Any(excf => excf(e));
+
+                    result = result & !this.AddCollection.Any(incf => incf(e));
+
+                    return result;
+                });
+
+                return _lc;
+            }
+
+            public LoggerConfiguration BuildIncludeOnly()
+            {
+                _lc.Filter.ByIncludingOnly(e =>
+                {
+                    bool result = this.AddCollection.Any(incf => incf(e));
+
+                    result = result & !this.RemoveCollection.Any(excf => excf(e));
+
+                    return result;
+                });
+
+                return _lc;
+            }
+        }
+    }
+}
